36e5a35ac9888b3a3bd5eb9790cba764c609d127
leonardoPimentelns <leonardooliveirapimentel@hotmail.com>
2022-07-13 13:08:23 -0300

    Update sensor.py

--- custom_components/xbox/sensor.py
+++ custom_components/xbox/sensor.py
@@ -1,47 +1,73 @@
 """Platform for sensor integration."""
 from __future__ import annotations
 
-from homeassistant.components.sensor import (
-    SensorDeviceClass,
-    SensorEntity,
-    SensorStateClass,
-)
-from homeassistant.helpers.event import async_track_time_interval
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
-from homeassistant import util
-import sys
-import pandas as pd
+import voluptuous as vol
 from datetime import timedelta
+import pandas as pd
+from homeassistant.components.sensor import (
+    SensorEntity
+)
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
+import homeassistant.helpers.config_validation as cv
+from homeassistant.core import callback
+from homeassistant.helpers.event import async_track_time_interval
+
+import sys
 from aiohttp import ClientSession
 from xbox.webapi.api.client import XboxLiveClient
 from xbox.webapi.authentication.manager import AuthenticationManager
 from xbox.webapi.authentication.models import OAuth2TokenResponse
 from xbox import *
 
-client_id = '590c180c-ec11-451b-b2b5-1d785d77403a'
-client_secret = 'mYt8Q~B2PGEUn.k.xuvftMUG_HmX6R-sqs3HwaAY'
-UPDATE_FREQUENCY = timedelta(seconds=1)
-TITLE_ID = None
+CLIENT_ID = 'client_id'
+CLIENT_SECRET = 'client_secret'
+TOKENS = 'tokens'
+UPDATE_FREQUENCY = timedelta(seconds=10)
+ICON = "mdi:microsoft-xbox"
 
-
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CLIENT_ID): cv.string,
+        vol.Required(CLIENT_SECRET): cv.string,
+        vol.Required(TOKENS): cv.string
+    }
+)
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the sensor platform."""
-    add_entities([CustomXbox()])
+    add_entities([CustomXbox(config)])
 
 
 class CustomXbox(SensorEntity):
     """Representation of a Sensor."""
-    def __init__(self):
+    def __init__(self,config):
         self._attr_name = "Xbox"
         self.results = None
+        self.config = config
+    @property
+    def should_poll(self):
+        """Return False as this entity has custom polling."""
+        return False
+    @property
+    def icon(self):
+        """Return the icon to use in the frontend."""
+        return ICON
+    async def async_added_to_hass(self):
+            """Start custom polling."""
 
-    @util.Throttle(UPDATE_FREQUENCY)
+
+            @callback
+            def async_update(event_time=None):
+                """Update the entity."""
+                self.async_schedule_update_ha_state(True)
+
+
+            async_track_time_interval(self.hass, async_update, UPDATE_FREQUENCY)
+
     async def async_update(self):
-        """Retrieve latest state."""
-        self.results =  await async_main()
+         self.results =  await async_main(self.config )
+         self._attr_native_value =  None
 
-        self._attr_native_value =  None
 
     @property
     def extra_state_attributes(self):
@@ -53,11 +79,11 @@ class CustomXbox(SensorEntity):
 
 
 
-async def async_main():
-    tokens_file = "/workspaces/core/homeassistant/components/custom_xbox/tokens.json" # replace with path in auth scrip or just paste file with tokens here
+async def async_main(config):
+    tokens_file = config[TOKENS] # replace with path in auth scrip or just paste file with tokens here
     async with ClientSession() as session:
         auth_mgr = AuthenticationManager(
-              session, client_id, client_secret, "")
+              session, CLIENT_ID, CLIENT_SECRET, "")
 
         try:
             with open(tokens_file, mode="r") as f:
